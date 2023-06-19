5c14cddd6e306e56427826fa18f7e9b10de2105e
Nick Novoa <nicknovoa03@gmail.com>
2023-06-19 17:39:46 -0500

    improve error handling

--- dist/src/bridge.js
+++ dist/src/bridge.js
@@ -9,34 +9,55 @@ const BridgeEth_json_1 = __importDefault(require("../artifacts/contracts/BridgeE
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 // Set private key for EthBridge Admin
-const EthBridgeAdminPrivateKey = process.env.ETH_PRIVATE_KEY;
+const EthBridgeAdminPrivateKey = process.env.ETH_TEST_PRIVATE_KEY;
 // Set Providers
-const EthProvider = new ethers_1.ethers.JsonRpcProvider("https://sepolia.infura.io/v3/" + process.env.INFURA_API_KEY);
-const BscProvider = new ethers_1.ethers.JsonRpcProvider("https://data-seed-prebsc-1-s1.binance.org:8545");
+let EthProvider = new ethers_1.ethers.JsonRpcProvider("https://infura.io/v3/" + process.env.INFURA_API_KEY);
+let BscProvider = new ethers_1.ethers.JsonRpcProvider("https://bsc-dataseed.binance.org/");
+// Set Providers
+let EthTestProvider = new ethers_1.ethers.JsonRpcProvider("https://sepolia.infura.io/v3/" + process.env.INFURA_API_KEY);
+let BscTestProvider = new ethers_1.ethers.JsonRpcProvider("https://data-seed-prebsc-1-s1.binance.org:8545");
+EthProvider = EthTestProvider;
+BscProvider = BscTestProvider;
 // Set Contract Addresses
-const EthBridgeAddress = "0xc14F24cf3768a0997E39aE11F288Ea421485EF54";
-const BscBridgeAddress = "0xc14F24cf3768a0997E39aE11F288Ea421485EF54";
+let EthBridgeAddress = "0xc14F24cf3768a0997E39aE11F288Ea421485EF54";
+let BscBridgeAddress = "0x9Af4010CCAFAd067ff8b961eC94371fc805823b5";
+// Set Contract Addresses
+let EthTestBridgeAddress = "0x0aaFfD6fb69cfc591e775c6A4D10Ab43b32bEBd1";
+;
+let BscTestBridgeAddress = "0xE12D1a1Abbc5f3a9FF0CdEeD2Bb238d86D6620A5";
+EthBridgeAddress = EthTestBridgeAddress;
+BscBridgeAddress = BscTestBridgeAddress;
 // Get the signer's address
 const signerEth = new ethers_1.ethers.Wallet(EthBridgeAdminPrivateKey, EthProvider);
 // Load Bridge Contracts
 const bridgeEth = new ethers_1.ethers.Contract(EthBridgeAddress, BridgeEth_json_1.default.abi, signerEth);
 const bridgeBsc = new ethers_1.ethers.Contract(BscBridgeAddress, BridgeBsc_json_1.default.abi, BscProvider);
 async function main() {
-    console.log("Initializing");
-    bridgeBsc.on("Transfer", async (from, to, amount, date, nonce, step) => {
-        console.log("Transfer Found!");
-        const tx = await bridgeEth.tokenTransfer(to, amount, nonce);
-        console.log(`Transaction hash: ${tx.hash}`);
-        console.log(`
-      Processed transfer:
-      - from ${from} 
-      - to ${to} 
-      - amount ${amount} tokens
-      - date ${date}
-      - nonce ${nonce}
-    `);
-        console.log("Searching...");
-    });
+    try {
+        console.log("Initializing");
+        bridgeBsc.on("Transfer", async (from, to, amount, date, nonce, step) => {
+            console.log("Transfer Found!");
+            const tx = await bridgeEth.tokenTransfer(to, amount, nonce);
+            console.log(`Transaction hash: ${tx.hash}`);
+            console.log(`
+          Processed transfer:
+          - from ${from} 
+          - to ${to} 
+          - amount ${amount} tokens
+          - date ${date}
+          - nonce ${nonce}
+        `);
+            console.log("Searching...");
+        });
+    }
+    catch (error) {
+        console.error(error);
+        // Restart the main function
+        main().catch((error) => {
+            console.error(error);
+            process.exitCode = 1;
+        });
+    }
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
--- src/bridge.ts
+++ src/bridge.ts
@@ -19,8 +19,15 @@ EthProvider = EthTestProvider
 BscProvider = BscTestProvider
 
 // Set Contract Addresses
-const EthBridgeAddress = "0x0aaFfD6fb69cfc591e775c6A4D10Ab43b32bEBd1";
-const BscBridgeAddress = "0xE12D1a1Abbc5f3a9FF0CdEeD2Bb238d86D6620A5";
+let EthBridgeAddress = "0xc14F24cf3768a0997E39aE11F288Ea421485EF54";
+let BscBridgeAddress = "0x9Af4010CCAFAd067ff8b961eC94371fc805823b5";
+
+// Set Contract Addresses
+let EthTestBridgeAddress = "0x0aaFfD6fb69cfc591e775c6A4D10Ab43b32bEBd1";;
+let BscTestBridgeAddress = "0xE12D1a1Abbc5f3a9FF0CdEeD2Bb238d86D6620A5";
+
+EthBridgeAddress = EthTestBridgeAddress;
+BscBridgeAddress = BscTestBridgeAddress;
 
 // Get the signer's address
 const signerEth = new ethers.Wallet(EthBridgeAdminPrivateKey!, EthProvider);
@@ -30,26 +37,33 @@ const bridgeEth = new ethers.Contract(EthBridgeAddress, BridgeEth.abi, signerEth
 
 const bridgeBsc = new ethers.Contract(BscBridgeAddress, BridgeBsc.abi, BscProvider);
 
-
 async function main() {
+    try {
+        console.log("Initializing");
+        bridgeBsc.on("Transfer", async (from, to, amount, date, nonce, step) => {
+            console.log("Transfer Found!");
 
-    console.log("Initializing");
-    bridgeBsc.on("Transfer", async (from, to, amount, date, nonce, step) => {
-        console.log("Transfer Found!");
+            const tx = await bridgeEth.tokenTransfer(to, amount, nonce);
 
-        const tx = await bridgeEth.tokenTransfer(to, amount, nonce);
-
-        console.log(`Transaction hash: ${tx.hash}`);
-        console.log(`
-      Processed transfer:
-      - from ${from} 
-      - to ${to} 
-      - amount ${amount} tokens
-      - date ${date}
-      - nonce ${nonce}
-    `);
-        console.log("Searching...");
-    });
+            console.log(`Transaction hash: ${tx.hash}`);
+            console.log(`
+          Processed transfer:
+          - from ${from} 
+          - to ${to} 
+          - amount ${amount} tokens
+          - date ${date}
+          - nonce ${nonce}
+        `);
+            console.log("Searching...");
+        });
+    } catch (error) {
+        console.error(error);
+        // Restart the main function
+        main().catch((error) => {
+            console.error(error);
+            process.exitCode = 1;
+        });
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -58,3 +72,4 @@ main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
 });
+
