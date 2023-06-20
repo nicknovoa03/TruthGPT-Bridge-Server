7666d7fc16aac1cb6672a40d9d9d07b28376710b
Nick Novoa <nicknovoa03@gmail.com>
2023-06-19 19:34:26 -0500

    add start and dev CLI

--- dist/src/bridge.js
+++ dist/src/bridge.js
@@ -8,25 +8,31 @@ const BridgeBsc_json_1 = __importDefault(require("../artifacts/contracts/BridgeB
 const BridgeEth_json_1 = __importDefault(require("../artifacts/contracts/BridgeEth.sol/BridgeEth.json"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+// parse input
+const args = process.argv.slice(2);
+const testing = args[0] === 'true'; // Example: Accept "true" as testing value
 // Set private key for EthBridge Admin
 let EthBridgeAdminPrivateKey = process.env.ETH_PRIVATE_KEY;
 let EthTestBridgeAdminPrivateKey = process.env.ETH_TEST_PRIVATE_KEY;
-//EthBridgeAdminPrivateKey = EthTestBridgeAdminPrivateKey;
 // Set Providers
 let EthProvider = new ethers_1.ethers.JsonRpcProvider("https://infura.io/v3/" + process.env.INFURA_API_KEY);
 let BscProvider = new ethers_1.ethers.JsonRpcProvider("https://bsc-dataseed.binance.org/");
 let EthTestProvider = new ethers_1.ethers.JsonRpcProvider("https://sepolia.infura.io/v3/" + process.env.INFURA_API_KEY);
 let BscTestProvider = new ethers_1.ethers.JsonRpcProvider("https://data-seed-prebsc-1-s1.binance.org:8545");
-//EthProvider = EthTestProvider
-//BscProvider = BscTestProvider
 // Set Contract Addresses
 let EthBridgeAddress = "0xc14F24cf3768a0997E39aE11F288Ea421485EF54";
 let BscBridgeAddress = "0x9Af4010CCAFAd067ff8b961eC94371fc805823b5";
 let EthTestBridgeAddress = "0x0aaFfD6fb69cfc591e775c6A4D10Ab43b32bEBd1";
 ;
 let BscTestBridgeAddress = "0xE12D1a1Abbc5f3a9FF0CdEeD2Bb238d86D6620A5";
-//EthBridgeAddress = EthTestBridgeAddress;
-//BscBridgeAddress = BscTestBridgeAddress;
+// check for test
+if (testing) {
+    EthBridgeAddress = EthTestBridgeAddress;
+    BscBridgeAddress = BscTestBridgeAddress;
+    EthBridgeAdminPrivateKey = EthTestBridgeAdminPrivateKey;
+    EthProvider = EthTestProvider;
+    BscProvider = BscTestProvider;
+}
 // Get the signer's address
 const signerEth = new ethers_1.ethers.Wallet(EthBridgeAdminPrivateKey, EthProvider);
 // Load Bridge Contracts
@@ -35,10 +41,11 @@ const bridgeBsc = new ethers_1.ethers.Contract(BscBridgeAddress, BridgeBsc_json_
 async function main() {
     try {
         console.log("Initializing");
+        console.log(testing);
         bridgeBsc.on("Transfer", async (from, to, amount, date, nonce, step) => {
             console.log("Transfer Found!");
             const tx = await bridgeEth.tokenTransfer(to, amount, nonce);
-            console.log(`Transaction hash: ${tx.hash}`);
+            console.log(`Eth Transaction hash: ${tx.hash}`);
             console.log(`
           Processed transfer:
           - from ${from} 
@@ -60,6 +67,7 @@ async function main() {
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
+// Parse command-line arguments to get the testing value
 main().catch(() => {
     process.exitCode = 1;
 });
--- src/bridge.ts
+++ src/bridge.ts
@@ -4,26 +4,36 @@ import BridgeEth from '../artifacts/contracts/BridgeEth.sol/BridgeEth.json'
 import dotenv from 'dotenv';
 dotenv.config();
 
+// parse input
+const args = process.argv.slice(2);
+const testing = args[0] === 'true'; // Example: Accept "true" as testing value
+
 // Set private key for EthBridge Admin
 let EthBridgeAdminPrivateKey = process.env.ETH_PRIVATE_KEY;
 let EthTestBridgeAdminPrivateKey = process.env.ETH_TEST_PRIVATE_KEY;
-//EthBridgeAdminPrivateKey = EthTestBridgeAdminPrivateKey;
 
 // Set Providers
 let EthProvider = new ethers.JsonRpcProvider("https://infura.io/v3/" + process.env.INFURA_API_KEY);
 let BscProvider = new ethers.JsonRpcProvider("https://bsc-dataseed.binance.org/");
 let EthTestProvider = new ethers.JsonRpcProvider("https://sepolia.infura.io/v3/" + process.env.INFURA_API_KEY);
 let BscTestProvider = new ethers.JsonRpcProvider("https://data-seed-prebsc-1-s1.binance.org:8545");
-//EthProvider = EthTestProvider
-//BscProvider = BscTestProvider
+
 
 // Set Contract Addresses
 let EthBridgeAddress = "0xc14F24cf3768a0997E39aE11F288Ea421485EF54";
 let BscBridgeAddress = "0x9Af4010CCAFAd067ff8b961eC94371fc805823b5";
 let EthTestBridgeAddress = "0x0aaFfD6fb69cfc591e775c6A4D10Ab43b32bEBd1";;
 let BscTestBridgeAddress = "0xE12D1a1Abbc5f3a9FF0CdEeD2Bb238d86D6620A5";
-//EthBridgeAddress = EthTestBridgeAddress;
-//BscBridgeAddress = BscTestBridgeAddress;
+
+// check for test
+if (testing) {
+    EthBridgeAddress = EthTestBridgeAddress;
+    BscBridgeAddress = BscTestBridgeAddress;
+    EthBridgeAdminPrivateKey = EthTestBridgeAdminPrivateKey;
+    EthProvider = EthTestProvider
+    BscProvider = BscTestProvider
+}
+
 
 // Get the signer's address
 const signerEth = new ethers.Wallet(EthBridgeAdminPrivateKey!, EthProvider);
@@ -36,12 +46,13 @@ const bridgeBsc = new ethers.Contract(BscBridgeAddress, BridgeBsc.abi, BscProvid
 async function main() {
     try {
         console.log("Initializing");
+        console.log(testing)
         bridgeBsc.on("Transfer", async (from, to, amount, date, nonce, step) => {
             console.log("Transfer Found!");
 
             const tx = await bridgeEth.tokenTransfer(to, amount, nonce);
 
-            console.log(`Transaction hash: ${tx.hash}`);
+            console.log(`Eth Transaction hash: ${tx.hash}`);
             console.log(`
           Processed transfer:
           - from ${from} 
@@ -63,6 +74,7 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
+// Parse command-line arguments to get the testing value
 main().catch(() => {
     process.exitCode = 1;
 });
